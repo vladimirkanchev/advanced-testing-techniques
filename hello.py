2d49f3ff5325a3d18dd7e0a51e95f672a2d71584
vladimirkanchev <kanchev.vladimir@gmail.com>
2022-06-15 11:08:18 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def addthis(x, y):
-  print(f"This is x {x} and the x-type is {type(x)}")
-  print(f"This is {y} and the y-type is {type(y)}")
+  print(f"This is x: {x} and the x-type is {type(x)}")
+  print(f"This is y: {y} and the y-type is {type(y)}")
   result = x+y
   print(f"This is the result {result}")
   return result
