e754777ac0c1f80183c687590c8c32fee049a59a
vladimirkanchev <kanchev.vladimir@gmail.com>
2022-06-15 11:19:04 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 def addthis(x, y):
   print(f"This is x: {x} and the x-type is {type(x)}")
   print(f"This is y: {y} and the y-type is {type(y)}")
-  result = x+y
+  try:
+    result = x+y
+  except TypeError as exception:
+    print(f"The wrong type passed in {exception}")
+    result = int(x)+int(y)
+    
   print(f"This is the result: {result}")
   return result
 
